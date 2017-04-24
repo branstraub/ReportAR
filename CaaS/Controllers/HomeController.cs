28de9d9240a4709cc383215e3556480b3435db21
Branko Straub <b.straub@outlook.com>
2017-04-24 17:58:25 -0300

    Refactor API, Upload File, GeoDecode GPS

--- CaaS/Controllers/HomeController.cs
+++ CaaS/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -10,6 +11,7 @@ using CaaS.Models;
 using CaaS.Models.BVModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.WindowsAzure.Storage;
 using Newtonsoft.Json;
 
 namespace CaaS.Controllers
@@ -33,6 +35,7 @@ namespace CaaS.Controllers
             return View();
         }
 
+        [HttpPost]
         public ActionResult CreateIssue(ReporteCreateModel model)
         {
             if (model.Descripcion == null || model.Id == null || model.Lat == null || model.Lon == null)
@@ -40,19 +43,63 @@ namespace CaaS.Controllers
                 return new HttpStatusCodeResult(400);
             }
 
-            var file = Request.Files[0];
+           
+            var urlpic = "";
+            if (Request.Files.Count != 0)
+            {
+                var file = Request.Files[0];
+
+                var storageAccount =
+                    CloudStorageAccount.Parse(
+                        "DefaultEndpointsProtocol=https;AccountName=abrigarpics;AccountKey=cWkQ94z1J22ZCZ4AT+17nOjXUPmE48qhqJREN4RJhM8giIfzP6hjeSr7DKgOcmK6rJ+lplF+av1Dqt2mp72CHA==;EndpointSuffix=core.windows.net");
+                var blobClient = storageAccount.CreateCloudBlobClient();
+                var container = blobClient.GetContainerReference("issuepic");
+
+                urlpic = model.Id + "-" + Guid.NewGuid();
+                var blockBlob = container.GetBlockBlobReference(urlpic);
+                using (var fileStream = file.InputStream)
+                {
+                    blockBlob.UploadFromStream(fileStream);
+                }
+            }
+            
+            var address = "";
+
+            try
+            {
+
+                var html = "";
+                var url = @"http://dev.virtualearth.net/REST/v1/Locations/{0},{1}?key=AvnNFjDsnWvQb6le63wIjKsNKdvu2PqntR1-vph6KkzXiAS2gj491wfkaoqinn1f";
+
+                url = url.Replace("{0}", model.Lat.ToString().Replace(",", ".")).Replace("{1}", model.Lon.ToString().Replace(",", "."));
+                var request = (HttpWebRequest)WebRequest.Create(url);
+               
+                using (var response = (HttpWebResponse)request.GetResponse())
+                using (var stream = response.GetResponseStream())
+                using (var reader = new StreamReader(stream))
+                {
+                    html = reader.ReadToEnd();
+                }
+
+                dynamic jsonDecoded = JsonConvert.DeserializeObject<dynamic>(html);
+                address = jsonDecoded.resourceSets[0].resources[0].address.formattedAddress;
+            }
+            catch (Exception e)
+            {
+                address = "";
+            }
 
             _reportesRepository.CreateReporte(new ReporteModel
             {
                 Id = Guid.NewGuid().ToString(),
                 Desc = model.Descripcion,
                 DateReported = DateTime.Now,
-                Direccion = "null", //TODO: llamar a la api de geodecode
+                Direccion = address,
                 Estado = 0,
                 Latitud = (double) model.Lat,
                 Longitud = (double) model.Lon,
                 ReportedBy = model.Id,
-                UrlPic = "null", //TODO: ver donde pija la guardo
+                UrlPic = urlpic.Any()? urlpic : "", 
             });
 
             return new HttpStatusCodeResult(201);
